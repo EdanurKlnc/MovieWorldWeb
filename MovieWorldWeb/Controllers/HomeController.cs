611d75fa155ce84bb8a0080190f5cdd6b11fb0a7
EdanurKlnc <edanur.kilinc@ogrenci.bartin.edu.tr>
2023-06-18 16:01:57 +0300

    Sayfalar düzenlendi.

--- MovieWorldWeb/Controllers/HomeController.cs
+++ MovieWorldWeb/Controllers/HomeController.cs
@@ -23,7 +23,15 @@ namespace MovieWorldWeb.Controllers
         {
             return View();
         }
-   
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         public IActionResult HomeUser()
         {
             try
