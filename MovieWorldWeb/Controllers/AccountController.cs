56b03081119ae756f75e615d55741ebc70d09864
EdanurKlnc <edanur.kilinc@ogrenci.bartin.edu.tr>
2023-06-21 00:27:25 +0300

    login ve register model eklendi

--- MovieWorldWeb/Controllers/AccountController.cs
+++ MovieWorldWeb/Controllers/AccountController.cs
@@ -5,31 +5,140 @@ using MovieWorldWebEL.IdentityModels;
 
 namespace MovieWorldWeb.Controllers
 {
-    public class AccountController : Controller
-    {
-        private readonly UserManager<AppUser> _userManager;
-        private readonly SignInManager<AppUser> _signInManager;
-        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager )
-        {
-            _userManager = userManager;
-    
-            _signInManager = signInManager;
-            
-        }
+	public class AccountController : Controller
+	{
+		private readonly UserManager<AppUser> _userManager;
+		private readonly SignInManager<AppUser> _signInManager;
+		public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+		{
+			_userManager = userManager;
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-        public IActionResult Register(RegisterViewModels model)
-        {
-            return View();
-        }
-        public IActionResult Login(LoginViewModels model)
-        {
-            return View();
-        }
-       
+			_signInManager = signInManager;
 
-    }
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Register()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Register(RegisterViewModels model)
+		{
+			try
+			{
+				if (!ModelState.IsValid)
+				{
+					return View(model);
+				}
+
+				// aynı username'den varsa hata versin
+				var sameUser = _userManager.FindByNameAsync(model.Username).Result; // async bir metodun sonuna .Result yazarsak metod senkron çalışır
+				if (sameUser != null)
+				{
+					ModelState.AddModelError("", "Bu kullanıcı ismi sistemde mevcuttur! Farklı kullanıcı adı deneyiniz!");
+				}
+
+
+				sameUser = _userManager.FindByEmailAsync(model.Email).Result;
+				if (sameUser != null)
+				{
+					ModelState.AddModelError("", "Bu email ile sistemde mevcuttur! Farklı email deneyiniz!");
+				}
+
+
+				AppUser user = new AppUser()
+				{
+					UserName = model.Username,
+					Name = model.Name,
+					Surname = model.Surname,
+					Email = model.Email,
+					EmailConfirmed = true,
+
+
+				};
+
+				var result = _userManager.CreateAsync(user, model.Password).Result;
+				if (result.Succeeded)
+				{				
+					return RedirectToAction("Login", "Account");
+				}
+				else
+				{
+					ModelState.AddModelError("", "Ekleme başarısız!");
+					foreach (var item in result.Errors)
+					{
+						ModelState.AddModelError("", item.Description);
+
+					}
+					return View(model);
+				}
+			}
+			catch (Exception ex)
+			{
+				ModelState.AddModelError("", "Beklenmedik hata oluştu!");
+				return View(model);
+
+			}
+		}
+
+
+		[HttpGet]
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Login(LoginViewModels model)
+		{
+			try
+			{
+				if (!ModelState.IsValid)
+				{
+					return View(model);
+				}
+
+				var user = _userManager.FindByEmailAsync(model.UserNameOrEmail).Result;
+
+				if (user == null)
+				{
+					ModelState.AddModelError("", "Email ya da şifre hatalidir!");
+					return View(model);
+				}
+				var signinResult =
+				 _signInManager.PasswordSignInAsync(user, model.Password, true, true).Result;
+				TempData["LoggedInUsername"] = user.UserName; //username sayisal deger olarak geliyor.
+				TempData["LoggedInNameSurname"] = $"{user.Name} {user.Surname}";
+
+				if (!signinResult.Succeeded)
+				{
+					ModelState.AddModelError("", "Giris BASARISIZDIR!");
+					return View(model);
+				}
+				if (_userManager.IsEmailConfirmedAsync(user).Result)
+				{
+					return RedirectToAction("Index", "HomeUser");
+				}
+				else
+				{
+					return RedirectToAction("Index", "HomeUser");
+				}
+				return View();
+			}
+			catch (Exception ex)
+			{
+				ModelState.AddModelError("", "Beklenmedik bir hata olustu!");
+				return View(model);
+			}
+		}
+
+
+	}
 }
--- MovieWorldWeb/Program.cs
+++ MovieWorldWeb/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using MovieWorldWeb;
+using MovieWorldWebEL.IdentityModels;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<MyContext>(options =>
@@ -8,8 +11,12 @@ builder.Services.AddDbContext<MyContext>(options =>
 
 });
 
+
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
 
 var app = builder.Build();
 
@@ -31,5 +38,12 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+using (var scope = app.Services.CreateScope())
+{
+	//Resolve ASP .NET Core Identity with DI help
+	var userManager = (UserManager<AppUser>?)scope.ServiceProvider.GetService(typeof(UserManager<AppUser>));
+
+}
 
 app.Run();
